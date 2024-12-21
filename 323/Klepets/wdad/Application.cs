eab2223dc640b64f2f94a0c5a40db2c63f8e144f
Klepets323 <Vikts05@mail.ru>
2024-12-21 03:07:48 +0300

    Added entry point

---
+++ 323/Klepets/wdad/Application.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace starting_monkey_to_human_path._323.Klepets.wdad
+{
+    public class Application
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("I’m Виктория Клепец, and I’m not a monkey");
+        }
+    }
+}
